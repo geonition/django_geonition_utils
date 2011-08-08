b116c0217c0221dd4bf602d8f75df3a7dc023838
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-08-08 13:17:37 +0300

    added a Http response for not implmeneted and modified a little bit so that the status code is actually returned correctly

--- geonition_utils/HttpResponseExtenders.py
+++ geonition_utils/HttpResponseExtenders.py
@@ -2,9 +2,13 @@ from django.http import HttpResponse
 from django.contrib.sites.models import Site
 
 class HttpResponseNotAuthorized(HttpResponse):
-    status_code = 401
 
     def __init__(self, message):
-        HttpResponse.__init__(self, message)
+        HttpResponse.__init__(self, message, status=401)
         #self['WWW-Authenticate'] = 'Basic realm="%s"' % Site.objects.get_current().name
 
+
+class HttpResponseNotImplemented(HttpResponse):
+    
+    def __init__(self, message):
+        HttpResponse.__init__(self, message, status=501)
