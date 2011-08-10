cf732f4810eb496716269b17dbb34a4282b7fafe
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-08-10 09:43:35 +0300

    NotAuthorized http response canged to Unauthorized like the http specification

--- geonition_utils/HttpResponseExtenders.py
+++ geonition_utils/HttpResponseExtenders.py
@@ -1,13 +1,22 @@
 from django.http import HttpResponse
 from django.contrib.sites.models import Site
 
-class HttpResponseNotAuthorized(HttpResponse):
+class HttpResponseCreated(HttpResponse):
+    
+    def __init__(self, message):
+        HttpResponse.__init__(self, message, status=201)
+        
+class HttpResponseUnauthorized(HttpResponse):
 
     def __init__(self, message):
         HttpResponse.__init__(self, message, status=401)
-        #self['WWW-Authenticate'] = 'Basic realm="%s"' % Site.objects.get_current().name
+        self['WWW-Authenticate'] = 'Basic realm="%s"' % Site.objects.get_current().name
 
-
+class HttpResponseConflict(HttpResponse):
+    
+    def __init__(self, message):
+        HttpResponse.__init__(self, message, status=409)
+        
 class HttpResponseNotImplemented(HttpResponse):
     
     def __init__(self, message):
