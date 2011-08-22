321442fb478a40cfecc6357cc9de7098c2f53fe6
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-08-22 11:21:35 +0200

    added some comments in the http extenders file

--- geonition_utils/HttpResponseExtenders.py
+++ geonition_utils/HttpResponseExtenders.py
@@ -2,12 +2,16 @@ from django.http import HttpResponse
 from django.contrib.sites.models import Site
 
 class HttpResponseCreated(HttpResponse):
-    
+    """
+    TODO: should return location of resource created
+    """
     def __init__(self, message):
         HttpResponse.__init__(self, message, status=201)
         
 class HttpResponseUnauthorized(HttpResponse):
-
+    """
+    TODO: should return www-autenticate with OAuth (implement OAuth first)
+    """
     def __init__(self, message):
         HttpResponse.__init__(self, message, status=401)
         #self['WWW-Authenticate'] = 'OAuth realm="%s"' % Site.objects.get_current().name
