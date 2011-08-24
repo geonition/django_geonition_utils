8893c5f5c2a02bc3a16ee1dc071773626b47b12b
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-08-24 22:39:55 +0300

    added setup.py script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='geonition_utils',
+    version='1.0.1',
+    author='Kristoffer Snabb',
+    url='https://github.com/geonition/django_geonition_utils',
+    packages=['geonition_utils'],
+)
