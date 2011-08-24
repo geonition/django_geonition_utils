65337d1136da0673415e89f9846f84a19620e5c9
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-08-24 23:13:06 +0300

    added dependencies into the setup.py file

--- setup.py
+++ setup.py
@@ -6,4 +6,7 @@ setup(
     author='Kristoffer Snabb',
     url='https://github.com/geonition/django_geonition_utils',
     packages=['geonition_utils'],
+    license="MIT-license",
+    requires=['pymongo',
+              'django'],
 )
