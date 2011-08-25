378a354ae2f0124ee8c73e354509d6b99dc9b97a
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-08-25 10:49:07 +0300

    added setup script

--- setup.py
+++ setup.py
@@ -2,11 +2,8 @@ from distutils.core import setup
 
 setup(
     name='geonition_utils',
-    version='1.0.1',
+    version='1.0.0',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/django_geonition_utils',
     packages=['geonition_utils'],
-    license="MIT-license",
-    install_requires = ['pymongo',
-                        'django'],
 )
