6574679ce3fd25ad6586fb278154f2ec6b2ff9a4
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-08-25 13:17:44 +0300

    requirements to setup file added

--- setup.py
+++ setup.py
@@ -6,4 +6,6 @@ setup(
     author='Kristoffer Snabb',
     url='https://github.com/geonition/django_geonition_utils',
     packages=['geonition_utils'],
+    install_requires=['django',
+                      'pymongo'],
 )
