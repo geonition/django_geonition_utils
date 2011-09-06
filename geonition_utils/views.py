4f64d40efe55fbf3fad63059ce541c1b18bfffc7
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-09-06 20:59:45 +0300

    class based view added, to be extended in the future

--- geonition_utils/views.py
+++ geonition_utils/views.py
@@ -1,1 +1,42 @@
-# Create your views here.
+"""
+This file contains view classes that can be used in other applications.
+"""
+from geonition_utils.HttpResponseExtenders import HttpResponseNotImplemented
+from django.views.generic import View
+
+class RequestHandler(View):
+    """
+    This class should be inherited by the view
+    classes to be used for implementing REST
+    """
+    def __init__(self):
+        pass
+    
+    def get(self, request, *args, **kwargs):
+        """
+        This method should be overridden
+        """
+        return HttpResponseNotImplemented("not implemented, yet!")
+    
+    def post(self, request, *args, **kwargs):
+        """
+        This method should be overridden
+        """
+        return HttpResponseNotImplemented("not implemented, yet!")
+    
+    def put(self, request, *args, **kwargs):
+        """
+        This method should be overridden
+        """
+        return HttpResponseNotImplemented("not implemented, yet!")
+    
+    def delete(self, request, *args, **kwargs):
+        """
+        This method should be overridden
+        """
+        return HttpResponseNotImplemented("not implemented, yet!")
+
+        
+        
+    
+    
