71c84a9dbd1fc256f4f4c7ea5923762a3ee8ed1c
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-09-08 16:09:31 +0300

    JSON model added with mongodb features and also timed model added, the timed model is not tested yet

--- geonition_utils/models.py
+++ geonition_utils/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.conf import settings
+from django.utils import simplejson as json
+from manager import MongoDBManager
+from datetime import datetime
 
-# Create your models here.
+class JSON(models.Model):
+    """
+    This model provides mongodb extensions
+    for json text field to query key value
+    pairs.
+    """
+    collection = models.CharField(max_length=20)
+    json_string = models.TextField()
+    
+    mongodb = MongoDBManager()
+    
+    def save(self, *args, **kwargs):
+        super(JSON, self).save(*args, **kwargs)
+        
+        #do nothing if USE_MONGODB False
+        if getattr(settings, "USE_MONGODB", False):
+            insert_json = json.loads(self.json_string)
+            JSON.mongodb.save(insert_json, self.id)
+    
+    def delete(self, *args, **kwargs):
+        super(JSON, self).save(*args, **kwargs)
+        
+        #do nothing if USE_MONGODB False
+        if getattr(settings, "USE_MONGODB", False):
+            JSON.mongodb.remove(self.id)
+      
+    def json(self):
+        return json.loads(self.json_string)
+        
+    def __unicode__(self):
+        return self.json_string
+    
+    
+    
+class TimeDModel(models.Model):
+    """
+    This model brings functionality to
+    query data that was 'valid' or
+    existing at a certain time.
+    """
+    create_time = models.DateTimeField(auto_now_add=True)
+    expire_time = models.DateTimeField(null=True)
+    
+    
