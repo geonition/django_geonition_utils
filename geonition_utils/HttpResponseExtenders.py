9a15c8890fe01ddab671b2bbd7b94a6959f96024
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-10-07 14:53:14 +0300

    the response objects now make sure that the response is in JSON format

--- geonition_utils/HttpResponseExtenders.py
+++ geonition_utils/HttpResponseExtenders.py
@@ -1,32 +1,75 @@
-from django.http import HttpResponse
+from django.http import HttpResponse as DjangoHttpResponse
+from django.http import HttpResponseForbidden as DjangoHttpResponseForbidden
+from django.http import HttpResponseNotFound as DjangoHttpResponseNotFound
 from django.contrib.sites.models import Site
 from django.utils import simplejson as json
 
-class HttpResponseCreated(HttpResponse):
+#200
+class HttpResponse(DjangoHttpResponse):
+    
+    def __init__(self, message):
+        DjangoHttpResponse.__init__(self,
+                                    message_to_json(message),
+                                    status=200,
+                                    mimetype='application/json')
+
+#201
+class HttpResponseCreated(DjangoHttpResponse):
     """
     TODO: should return location of resource created
     """
     def __init__(self, message):
-        HttpResponse.__init__(self, message_to_json(message), status=201)
+        DjangoHttpResponse.__init__(self,
+                                    message_to_json(message),
+                                    status=201,
+                                    mimetype='application/json')
+
+
+#403    
+class HttpResponseForbidden(DjangoHttpResponseForbidden):
+    
+    def __init__(self, message):
+        DjangoHttpResponseForbidden.__init__(self,
+                                             message_to_json(message),
+                                             mimetype='application/json')
+
+#404
+class HttpResponseNotFound(DjangoHttpResponse):
+    
+    def __init__(self, message):
+        DjangoHttpResponse.__init__(self,
+                                    message_to_json(message),
+                                    statue=404,
+                                    mimetype='application/json')
+
         
-class HttpResponseUnauthorized(HttpResponse):
+class HttpResponseUnauthorized(DjangoHttpResponse):
     """
     TODO: should return www-autenticate with OAuth (implement OAuth first)
     """
     def __init__(self, message):
         
-        HttpResponse.__init__(self, message_to_json(message), status=401)
+        DjangoHttpResponse.__init__(self,
+                                    message_to_json(message),
+                                    status=401,
+                                    mimetype='application/json')
         #self['WWW-Authenticate'] = 'OAuth realm="%s"' % Site.objects.get_current().name
 
-class HttpResponseConflict(HttpResponse):
+class HttpResponseConflict(DjangoHttpResponse):
     
     def __init__(self, message):
-        HttpResponse.__init__(self, message_to_json(message), status=409)
+        DjangoHttpResponse.__init__(self,
+                                    message_to_json(message),
+                                    status=409,
+                                    mimetype='application/json')
         
-class HttpResponseNotImplemented(HttpResponse):
+class HttpResponseNotImplemented(DjangoHttpResponse):
     
     def __init__(self, message):
-        HttpResponse.__init__(self, message_to_json(message), status=501)
+        DjangoHttpResponse.__init__(self,
+                                    message_to_json(message),
+                                    status=501,
+                                    mimetype='application/json')
         
         
 def message_to_json(message):
