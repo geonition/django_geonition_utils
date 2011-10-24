c1379b01fe344274a44da3b7061c4eb2f9dcea44
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-10-24 15:12:45 +0300

    changed version number, some better way to merge without always having to change the version number afterwards has to exist

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='geonition_utils',
-    version='1.0.x',
+    version='2.0.0',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/django_geonition_utils',
     packages=['geonition_utils'],
